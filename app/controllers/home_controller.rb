6389c620eda555f56695994f2fd5a02155a79514
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2017-05-22 13:15:32 -0700

    Renamed app to RateLimiter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
   private
 
   def check_rate_limit
-    rate_limiter = RateLimiter.new(ip_address: request.remote_ip)
+    rate_limiter = LimitRequestRate.new(ip_address: request.remote_ip)
     render plain: rate_limiter.time_until_next_request_permitted, status: 429 unless rate_limiter.request_permitted?
   end
 
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails51example
+module RateLimiter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
